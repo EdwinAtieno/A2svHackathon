c5afdca650f47c9d04f526fb4d9cff4a4daecaba
Chalo1996 <emusyoka759@gmail.com>
2023-09-02 19:56:20 +0300

    Integrate websocket

--- a2svhackathon/asgi.py
+++ a2svhackathon/asgi.py
@@ -10,7 +10,25 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import\
+    AllowedHostsOriginValidator
+
+from apps.chat.consumers import ChatConsumer
+from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a2svhackathon.settings')
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+                path('ws/chat/<str:room_name>/', ChatConsumer.as_asgi()),
+            ])
+        )
+    ),
+})
--- a2svhackathon/settings.py
+++ a2svhackathon/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import read_dotenv
 
+
+# read_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+read_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # ThirdParty
+    'channels',
+    'apps.chat'
 ]
 
 MIDDLEWARE = [
@@ -69,6 +77,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'a2svhackathon.wsgi.application'
+ASGI_APPLICATION = 'a2svhackathon.asgi.application'
 
 
 # Database
@@ -76,8 +85,12 @@ WSGI_APPLICATION = 'a2svhackathon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
