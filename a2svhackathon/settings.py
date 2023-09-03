17769785c41ef4d192bd540489c07db001464f62
Emmanuel Chalo <81255211+Chalo1996@users.noreply.github.com>
2023-09-03 15:17:30 +0300

    Update settings.py

--- a2svhackathon/settings.py
+++ a2svhackathon/settings.py
@@ -111,16 +111,6 @@ ASGI_APPLICATION = "a2svhackathon.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}"""
 # db_config = dj_database_url.config(default=config("DATABASE_URL"))
 # DATABASES = {"default": db_config}
 
