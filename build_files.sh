870ee7aa23babc7543f8f1ba32a420626df64ab9
Chalo1996 <emusyoka759@gmail.com>
2023-10-13 01:39:40 +0300

    Remove wrong command

--- build_files.sh
+++ build_files.sh
@@ -9,4 +9,4 @@ python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
 echo "Collecting static files..."
-python3 manage.py collectstatic --noinput clear
+python3 manage.py collectstatic --noinput
