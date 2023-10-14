3caca5af446409c16bd6e5a74e519e348db945a3
Chalo1996 <emusyoka759@gmail.com>
2023-10-15 00:06:01 +0300

    Comfigure runtime

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Building project packages..."
-python3 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Migrating Database..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+python manage.py migrate
 
 echo "Collecting static files..."
-python3 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput
+
+echo "Starting the application..."
+gunicorn a2svhackathon.wsgi --bind 0.0.0.0:$PORT
