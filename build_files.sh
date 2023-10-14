cca4ea0291ad7110bc25db913825b8563aaba080
Chalo1996 <emusyoka759@gmail.com>
2023-10-15 01:40:28 +0300

    Fix deployment

--- build_files.sh
+++ build_files.sh
@@ -10,5 +10,5 @@ python3 manage.py migrate --noinput
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
 
-#echo "Starting the application..."
-#gunicorn a2svhackathon.wsgi --bind 0.0.0.0:$PORT
+echo "Starting the application..."
+gunicorn a2svhackathon.wsgi:application --bind 0.0.0.0:$PORT
