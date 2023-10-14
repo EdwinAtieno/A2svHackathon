1d49fcdc22d9741d4ad1ef8207599a09f6ed0459
Chalo1996 <emusyoka759@gmail.com>
2023-10-15 01:30:51 +0300

    Fix deployment

--- apps/chat/admin.py
+++ apps/chat/admin.py
@@ -5,7 +5,7 @@ class ChatMessageAdmin(admin.ModelAdmin):
     list_display = ('user', 'timestamp', 'user_message', 'model_response')
     list_filter = ('user', 'timestamp')
     search_fields = ('user__email', 'user_message', 'model_response')
-    list_display_links = ('timestamp_display',)
+    list_display_links = ('timestamp',)
 
     def timestamp_display(self, obj):
         return obj.timestamp.strftime("%Y-%m-%d %H:%M:%S")
